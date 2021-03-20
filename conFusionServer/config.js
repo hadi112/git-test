332f59879d93673f87f751a979a118daf3545bb9
Hadi Husain <hadihusain112@gmail.com>
2021-03-20 15:51:28 +0530

    Delete config.js

--- conFusionServer/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	'secretKey':'1234567890',
-	'mongoUrl':'mongodb://localhost:27017/conFusion',
-	'facebook':{
-		clientId:'291002845778188',
-		clientSecret: 'ff62f21246e61d2db0bc71fa47694915'
-	}
-}
